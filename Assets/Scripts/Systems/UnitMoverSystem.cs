7413e0b7a93b15e26e59ef53f119ef3104159111
yoon20002000 <yoon20002000@naver.com>
2025-03-14 00:05:29 +0900

    Physics Component

--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -1,6 +1,7 @@
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Transforms;
 
 partial struct UnitMoverSystem : ISystem
@@ -8,9 +9,18 @@ partial struct UnitMoverSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach ( (RefRW<LocalTransform> localTransform, RefRO<MoveSpeed> moveSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>>())
+        foreach ((RefRW<LocalTransform> localTransform, RefRO<MoveSpeed> moveSpeed, RefRW<PhysicsVelocity> physicsVelocity) 
+                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>, RefRW<PhysicsVelocity>>())
         {
-            localTransform.ValueRW.Position += new float3(moveSpeed.ValueRO.moveSpeed, 0, 0) * SystemAPI.Time.DeltaTime;
+            float3 targetPosition = localTransform.ValueRO.Position + new float3(10, 0, 0);
+            float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
+            moveDirection = math.normalize(moveDirection);
+            
+            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.moveSpeed;
+            physicsVelocity.ValueRW.Angular = float3.zero;
+            
+            //localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.moveSpeed * SystemAPI.Time.DeltaTime;
         }
     }
 }
