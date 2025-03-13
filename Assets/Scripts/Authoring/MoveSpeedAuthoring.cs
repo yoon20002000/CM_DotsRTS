5b425c11cda2b7f16ed85a4f9db554975f86e5ae
yoon20002000 <yoon20002000@naver.com>
2025-03-13 23:21:04 +0900

    Entity, Component

---
+++ Assets/Scripts/Authoring/MoveSpeedAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public struct MoveSpeed : IComponentData
+{
+    public float moveSpeed;
+}
+
+public class MoveSpeedAuthoring : MonoBehaviour
+{
+    public float moveSpeed;
+    class Baker : Baker<MoveSpeedAuthoring>
+    {
+        public override void Bake(MoveSpeedAuthoring authoring)
+        {
+            Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+
+            AddComponent(entity, new MoveSpeed { moveSpeed = authoring.moveSpeed });
+        }
+    }
+}
