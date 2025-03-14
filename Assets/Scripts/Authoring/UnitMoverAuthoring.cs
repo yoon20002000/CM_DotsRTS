5794a1b52582a48c4062d50ce1d73f6a38a96584
yoon20002000 <yoon20002000@naver.com>
2025-03-14 17:25:21 +0900

    Moving Player Unit

---
+++ Assets/Scripts/Authoring/UnitMoverAuthoring.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct UnitMover : IComponentData
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    public float3 targetPosition; 
+}
+
+public class UnitMoverAuthoring : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    class Baker : Baker<UnitMoverAuthoring>
+    {
+        public override void Bake(UnitMoverAuthoring authoring)
+        {
+            Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+
+            AddComponent(entity, new UnitMover
+            {
+                moveSpeed = authoring.moveSpeed,
+                rotationSpeed =  authoring.rotationSpeed,
+            });
+        }
+    }
+}
--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -3,24 +3,26 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
+using UnityEngine;
 
 partial struct UnitMoverSystem : ISystem
 {
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach ((RefRW<LocalTransform> localTransform, RefRO<MoveSpeed> moveSpeed, RefRW<PhysicsVelocity> physicsVelocity) 
-                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>, RefRW<PhysicsVelocity>>())
+        foreach ((RefRW<LocalTransform> localTransform, RefRO<UnitMover> unitMover, RefRW<PhysicsVelocity> physicsVelocity) 
+                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<UnitMover>, RefRW<PhysicsVelocity>>())
         {
-            float3 targetPosition = MouseWorldPosition.Instance.GetPosition();
-            float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
+            float3 moveDirection = unitMover.ValueRO.targetPosition - localTransform.ValueRO.Position;
+            if (moveDirection is { x: 0, y: 0, z: 0 })
+            {
+                return;
+            }
             moveDirection = math.normalize(moveDirection);
-
-            float rotationSpeed = 10;
             
-            localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation,quaternion.LookRotation(moveDirection, math.up()), SystemAPI.Time.DeltaTime * rotationSpeed);
+            localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation,quaternion.LookRotation(moveDirection, math.up()), SystemAPI.Time.DeltaTime * unitMover.ValueRO.rotationSpeed);
             
-            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.moveSpeed;
+            physicsVelocity.ValueRW.Linear = moveDirection * unitMover.ValueRO.moveSpeed;
             physicsVelocity.ValueRW.Angular = float3.zero;
             
             //localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.moveSpeed * SystemAPI.Time.DeltaTime;
